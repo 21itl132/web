13b818738ff320d8dfbc0972ff086ba7412ae705
21itl132 <118598761+21itl132@users.noreply.github.com>
2023-11-07 20:59:35 -0800

    Add files via upload

---
+++ src/firebase/Firebase.config.js
@@ -0,0 +1,14 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBHl4go-Uag1tPWuyROTLe3dmsOOfp0JUw",
+  authDomain: "ecommerce-website-e23d3.firebaseapp.com",
+  databaseURL: "https://ecommerce-website-e23d3-default-rtdb.firebaseio.com",
+  projectId: "ecommerce-website-e23d3",
+  storageBucket: "ecommerce-website-e23d3.appspot.com",
+  messagingSenderId: "123263184815",
+  appId: "1:123263184815:web:260deb24963f436375c95a",
+  measurementId: "G-N16Q5ZRPZD"
+};
+
+  
+export default firebaseConfig
